99eb52ae14c97c00973f559cebccd589b1126225
Alvaro Padilla <monster_what@hotmail.com>
2026-08-21 04:53:05 -0600

    Remove transactional scope from TipoCambio API fetch

--- mercurius-quarkus/src/main/java/Services/TipoCambioService.java
+++ mercurius-quarkus/src/main/java/Services/TipoCambioService.java
@@ -35,7 +35,6 @@ public class TipoCambioService extends GService<TipoCambio> {
         return TipoCambio.class;
     }
 
-    @Transactional
     public void getTipoCambioFromApi() {
         
         LocalDateTime currentDateTime = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
